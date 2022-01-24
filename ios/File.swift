5eaf33de15f64d3188fc7954097535b8613461fa
OhJaeyeop <ohbcj0831@gmail.com>
2022-01-24 21:18:49 +0900

    내 위치 가져오기

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  HogangNoNo
+//
+//  Created by Mac on 2022/01/24.
+//
+
+import Foundation
--- src/components/HomeMap.tsx
+++ src/components/HomeMap.tsx
@@ -1,28 +1,77 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   View,
   Text,
-  SafeAreaView,
   Platform,
   StatusBar,
+  PermissionsAndroid,
 } from 'react-native';
-import NaverMapView from 'react-native-nmap';
+import NaverMapView, {Marker} from 'react-native-nmap';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
+import Geolocation from 'react-native-geolocation-service';
 
+type Location = {
+  latitude: number;
+  longitude: number;
+};
 const statusBarHeight =
   Platform.OS === 'ios' ? getStatusBarHeight(true) : StatusBar.currentHeight;
 
 const HomeMap = () => {
+  const [location, setLocation] = useState<Location | undefined>(undefined);
+
+  async function requestPermission() {
+    try {
+      if (Platform.OS === 'ios') {
+        return await Geolocation.requestAuthorization('always');
+      }
+      if (Platform.OS === 'android') {
+        return await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    requestPermission().then(result => {
+      if (result === 'granted') {
+        Geolocation.getCurrentPosition(
+          position => {
+            const {latitude, longitude} = position.coords;
+            setLocation({
+              latitude,
+              longitude,
+            });
+          },
+          error => {
+            console.log(error.code, error.message);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 1000},
+        );
+      }
+    });
+  }, []);
+
   return (
     <View style={{flex: 1}}>
       <View style={styles.statusBar} />
       <View style={styles.header}>
         <Text>HogangNoNo</Text>
       </View>
-      <NaverMapView
-        style={{width: '100%', height: '90%'}}
-        zoomControl={false}></NaverMapView>
+      {location ? (
+        <NaverMapView
+          style={{width: '100%', height: '90%'}}
+          center={{...location, zoom: 16}}
+          zoomControl={false}>
+          <Marker coordinate={location} />
+        </NaverMapView>
+      ) : (
+        <Text>Loading..</Text>
+      )}
     </View>
   );
 };
