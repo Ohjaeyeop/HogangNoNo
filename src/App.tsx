b74e7168d2d56007ae164245c228c6db196f8031
OhJaeyeop <ohbcj0831@gmail.com>
2022-01-31 10:39:38 +0900

    react-native-navigation 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, {useEffect} from 'react';
 import Home from './components/Home';
 import * as db from './db/db';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   useEffect(() => {
     db.init();
   }, []);
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  );
 };
 
 export default App;
