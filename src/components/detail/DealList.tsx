5b3f0c20d803997c9399503ecaa81cd646a174f2
OhJaeyeop <ohbcj0831@gmail.com>
2022-02-03 11:59:03 +0900

    DealList ScrollView로 변경

--- src/components/detail/DealList.tsx
+++ src/components/detail/DealList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {ResultSetRowList} from 'react-native-sqlite-storage';
 
 const DealList = ({
@@ -23,41 +23,47 @@ const DealList = ({
           <Text>층</Text>
         </View>
       </View>
-      {Array.from({length: dealInfoList.length}, (v, i) => i).map(index => {
-        const amount1 = Math.floor(dealInfoList.item(index).dealAmount / 10000);
-        const amount2 = dealInfoList.item(index).dealAmount % 10000;
-        const displayedAmount1 = amount1 > 0 ? `${amount1}억` : '';
-        const displayedAmount2 =
-          amount2 >= 1000
-            ? amount2.toString().slice(0, 1) + ',' + amount2.toString().slice(1)
-            : amount2 > 0
-            ? amount2.toString()
-            : '';
-        return (
-          <View style={styles.tableBody} key={index}>
-            <View style={styles.column}>
-              <Text>
-                {dealInfoList.item(index).year}.
-                {dealInfoList.item(index).month < 10
-                  ? `0${dealInfoList.item(index).month}`
-                  : dealInfoList.item(index).month}
-                .
-                {dealInfoList.item(index).day < 10
-                  ? `0${dealInfoList.item(index).day}`
-                  : dealInfoList.item(index).day}
-              </Text>
+      <ScrollView style={{width: '100%', height: '50%'}}>
+        {Array.from({length: dealInfoList.length}, (v, i) => i).map(index => {
+          const amount1 = Math.floor(
+            dealInfoList.item(index).dealAmount / 10000,
+          );
+          const amount2 = dealInfoList.item(index).dealAmount % 10000;
+          const displayedAmount1 = amount1 > 0 ? `${amount1}억` : '';
+          const displayedAmount2 =
+            amount2 >= 1000
+              ? amount2.toString().slice(0, 1) +
+                ',' +
+                amount2.toString().slice(1)
+              : amount2 > 0
+              ? amount2.toString()
+              : '';
+          return (
+            <View style={styles.tableBody} key={index}>
+              <View style={styles.column}>
+                <Text>
+                  {dealInfoList.item(index).year}.
+                  {dealInfoList.item(index).month < 10
+                    ? `0${dealInfoList.item(index).month}`
+                    : dealInfoList.item(index).month}
+                  .
+                  {dealInfoList.item(index).day < 10
+                    ? `0${dealInfoList.item(index).day}`
+                    : dealInfoList.item(index).day}
+                </Text>
+              </View>
+              <View style={styles.column}>
+                <Text style={{fontWeight: 'bold'}}>
+                  {displayedAmount1 + ' ' + displayedAmount2}
+                </Text>
+              </View>
+              <View style={styles.column}>
+                <Text>{dealInfoList.item(index).floor}층</Text>
+              </View>
             </View>
-            <View style={styles.column}>
-              <Text style={{fontWeight: 'bold'}}>
-                {displayedAmount1 + ' ' + displayedAmount2}
-              </Text>
-            </View>
-            <View style={styles.column}>
-              <Text>{dealInfoList.item(index).floor}층</Text>
-            </View>
-          </View>
-        );
-      })}
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
