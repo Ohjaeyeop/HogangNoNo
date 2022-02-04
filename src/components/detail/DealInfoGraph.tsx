474084e27c8f597a987d080b21581d16963782ab
OhJaeyeop <ohbcj0831@gmail.com>
2022-02-04 14:47:02 +0900

    tooltip 텍스트

--- src/components/detail/DealInfoGraph.tsx
+++ src/components/detail/DealInfoGraph.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Dimensions, StyleSheet, Text} from 'react-native';
 import {ResultSetRowList} from 'react-native-sqlite-storage';
 import Svg, {Path} from 'react-native-svg';
@@ -6,28 +6,50 @@ import {getGraphData} from '../../libs/getGraphData';
 import {color} from '../../theme/color';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 import Animated, {
+  runOnJS,
   useAnimatedGestureHandler,
+  useAnimatedReaction,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
 } from 'react-native-reanimated';
 import {getGraphPath} from '../../libs/getGraphPath';
+import {displayedAmount} from '../../libs/displayedAmount';
 
 const graphWidth = Dimensions.get('window').width - 40 - 40;
 const graphHeight = graphWidth * 0.4;
 const radius = 5;
 
 const DealInfoGraph = ({dealInfoGroup}: {dealInfoGroup: ResultSetRowList}) => {
-  const arr = getGraphData(dealInfoGroup);
+  const [tooltipText, setTooltipText] = useState('');
+  const graphData = getGraphData(dealInfoGroup);
   const maxValue = Math.ceil(
-    Math.max(...arr.map(value => value.amount)) / 10000,
+    Math.max(...graphData.map(value => value.amount)) / 10000,
   );
   const minValue = Math.floor(
-    Math.min(...arr.map(value => value.amount).filter(value => value !== 0)) /
-      10000,
+    Math.min(
+      ...graphData.map(value => value.amount).filter(value => value !== 0),
+    ) / 10000,
   );
-  const path = getGraphPath(maxValue, minValue, graphWidth, graphHeight, arr);
+  const gap = graphWidth / 36;
+  const path = getGraphPath(maxValue, minValue, gap, graphHeight, graphData);
+
   const x = useSharedValue(graphWidth);
-  const y = useSharedValue(0);
+  const dataIndex = useDerivedValue(() => {
+    return Math.min(
+      Math.max(Math.floor(x.value / gap), 0),
+      graphData.length - 1,
+    );
+  });
+
+  useAnimatedReaction(
+    () => dataIndex.value,
+    index => {
+      runOnJS(setTooltipText)(
+        `${graphData[index].year}.${graphData[index].month} 평균 ${graphData[index].displayedAmount} (${graphData[index].count}건)`,
+      );
+    },
+  );
 
   const circleAnimatedStyle = useAnimatedStyle(() => {
     return {
@@ -37,11 +59,12 @@ const DealInfoGraph = ({dealInfoGroup}: {dealInfoGroup: ResultSetRowList}) => {
 
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (event, ctx: any) => {
-      x.value = event.x - 20;
+      x.value = Math.min(Math.max(event.x - 20, -radius), graphWidth + radius);
+      ctx.startX = x.value;
     },
     onActive: (event, ctx: any) => {
       x.value = Math.min(
-        Math.max(x.value + event.translationX, -radius),
+        Math.max(ctx.startX + event.translationX, -radius),
         graphWidth + radius,
       );
     },
@@ -55,9 +78,7 @@ const DealInfoGraph = ({dealInfoGroup}: {dealInfoGroup: ResultSetRowList}) => {
         </Svg>
         <Animated.View style={[styles.circle, circleAnimatedStyle]} />
         <Animated.View style={styles.tooltip}>
-          <Text style={{color: 'white', fontWeight: '500'}}>
-            2021.10 평균 55억 (0건)
-          </Text>
+          <Text style={{color: 'white', fontWeight: '500'}}>{tooltipText}</Text>
         </Animated.View>
       </Animated.View>
     </PanGestureHandler>
