5bdea35aca942bd4120a2291ec877bcf1c172cc7
OhJaeyeop <ohbcj0831@gmail.com>
2022-02-08 11:07:04 +0900

    그래프 로딩 처리, 거래수 0인 경우 처리

--- src/components/detail/DealInfo.tsx
+++ src/components/detail/DealInfo.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   ActivityIndicator,
-  Alert,
   Share,
   StyleSheet,
   Text,
--- src/components/detail/DealInfoGraph.tsx
+++ src/components/detail/DealInfoGraph.tsx
@@ -36,6 +36,7 @@ const DealInfoGraph = ({dealInfoGroup, type, loading}: Props) => {
   const [tooltipWidth, setTooltipWidth] = useState(0);
   const arr = useMemo(() => [0, 1, 2, 3], []);
   const graphData = getGraphData(dealInfoGroup);
+
   const maxValue = Math.ceil(
     Math.max(...graphData.map(value => value.amount)) / 10000,
   );
@@ -150,40 +151,52 @@ const DealInfoGraph = ({dealInfoGroup, type, loading}: Props) => {
               />
             )}
           </Svg>
-          <Animated.View style={[styles.line, lineAnimatedStyle]} />
-          <Animated.View
-            style={[
-              styles.circle,
-              circleAnimatedStyle,
-              {backgroundColor: type === 'Deal' ? color.main : '#3D9752'},
-            ]}
-          />
-          <Animated.View
-            style={[styles.tooltip, tooltipAnimatedStyle]}
-            onLayout={event => setTooltipWidth(event.nativeEvent.layout.width)}>
-            <Text style={{color: 'white', fontWeight: '500'}}>
-              {tooltipText}
-            </Text>
-          </Animated.View>
+          {!loading && (
+            <Animated.View style={[styles.line, lineAnimatedStyle]} />
+          )}
+          {!loading && (
+            <Animated.View
+              style={[
+                styles.circle,
+                circleAnimatedStyle,
+                {backgroundColor: type === 'Deal' ? color.main : '#3D9752'},
+              ]}
+            />
+          )}
+          {!loading && (
+            <Animated.View
+              style={[styles.tooltip, tooltipAnimatedStyle]}
+              onLayout={event =>
+                setTooltipWidth(event.nativeEvent.layout.width)
+              }>
+              <Text style={{color: 'white', fontWeight: '500'}}>
+                {tooltipText}
+              </Text>
+            </Animated.View>
+          )}
           <View style={styles.chartContainer}>
-            {graphData.map((data, index) => (
-              <Animated.View
-                key={index}
-                style={{
-                  position: 'absolute',
-                  bottom: 0,
-                  left: -gap / 4 + gap * index,
-                  width: gap / 2,
-                  height: ((data.count / maxCount) * chartHeight) / 1.5,
-                  backgroundColor:
-                    dataIndex.value === index
-                      ? type === 'Deal'
-                        ? color.main
-                        : '#3D9752'
-                      : 'darkgray',
-                }}
-              />
-            ))}
+            {!loading &&
+              graphData.map((data, index) => (
+                <Animated.View
+                  key={index}
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: -gap / 4 + gap * index,
+                    width: gap / 2,
+                    height:
+                      maxCount > 0
+                        ? ((data.count / maxCount) * chartHeight) / 1.5
+                        : 0,
+                    backgroundColor:
+                      dataIndex.value === index
+                        ? type === 'Deal'
+                          ? color.main
+                          : '#3D9752'
+                        : 'darkgray',
+                  }}
+                />
+              ))}
           </View>
         </Animated.View>
       </PanGestureHandler>
